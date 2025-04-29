afb638fdc402253a4305de92eb4530beb3a58b4c
StudentPP1 <mishamakytonin@gmail.com>
2025-04-29 20:47:35 +0300

    add 2849. Determine if a Cell Is Reachable at a Given Time

---
+++ src/main/java/org/example/meduim/determineifaCellIsReachableataGivenTime/Main.java
@@ -0,0 +1,9 @@
+package org.example.meduim.determineifaCellIsReachableataGivenTime;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isReachableAtTime(2, 4, 7, 7, 6));
+        System.out.println(solution.isReachableAtTime(3, 1, 7, 3, 3));
+    }
+}
---
+++ src/main/java/org/example/meduim/determineifaCellIsReachableataGivenTime/Solution.java
@@ -0,0 +1,11 @@
+package org.example.meduim.determineifaCellIsReachableataGivenTime;
+
+public class Solution {
+    public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        if (sx == fx && sy == fy && t != 1) return true;
+        if (sx == fx && sy == fy) return false;
+        if (t == 0) return false;
+        // ! Chebyshev distance
+        return Math.max(Math.abs(sx - fx), Math.abs(sy - fy)) <= t;
+    }
+}
