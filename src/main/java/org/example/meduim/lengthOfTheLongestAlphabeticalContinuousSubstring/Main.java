17243738ecbe4a06db9d0026c825fcea506629e0
StudentPP1 <mishamakytonin@gmail.com>
2025-06-19 18:47:16 +0300

    2414. Length of the Longest Alphabetical Continuous Substring

---
+++ src/main/java/org/example/meduim/lengthOfTheLongestAlphabeticalContinuousSubstring/Main.java
@@ -0,0 +1,10 @@
+package org.example.meduim.lengthOfTheLongestAlphabeticalContinuousSubstring;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestContinuousSubstring("abacaba")); // 2
+        System.out.println(solution.longestContinuousSubstring("abcde")); // 5
+        System.out.println(solution.longestContinuousSubstring("yrpjofyzubfsiypfre")); // 2
+    }
+}
---
+++ src/main/java/org/example/meduim/lengthOfTheLongestAlphabeticalContinuousSubstring/Solution.java
@@ -0,0 +1,22 @@
+package org.example.meduim.lengthOfTheLongestAlphabeticalContinuousSubstring;
+
+class Solution {
+    public int longestContinuousSubstring(String s) {
+        int maxCount = 1;
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            // B (66) - A (65) = 1
+            if (s.charAt(i) - s.charAt(i - 1) == 1) {
+                count++;
+            }
+            else {
+                count = 1;
+            }
+            if (maxCount < count) {
+                maxCount = count;
+            }
+        }
+        return maxCount;
+    }
+
+}
